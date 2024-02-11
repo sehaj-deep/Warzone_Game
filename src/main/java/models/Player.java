47291fe76644a32eae053350cd45dcc67d5fe418
nitin <nitin.birdi@icloud.com>
2024-02-11 18:01:37 -0500

    implemented remove player method and other changes

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -5,27 +5,54 @@ import java.util.List;
 
 public class Player {
 
-    /* Each player name adding to d_playerName list */
+    // each player name adding to d_playerName list
     private final static List<Player> d_playerNameList = new ArrayList<>();
 
-    /**/
+    // getting each player name using this string
     public String d_playerName;
 
-    /* Defining constructor for player class */
+    // defining constructor for player class
     public Player(String d_playerName) {
         this.d_playerName = d_playerName;
     }
 
-    /* getting player name */
+    // getting player name
     public String get_playerName() {
         return d_playerName;
     }
 
-    /* Method to Add player */
+    // method to Add player name
     public void setD_PlayerName(String d_playerName) {
-      Player name = new Player(d_playerName);
-      d_playerNameList.add(name);
-      System.out.println("Player name "+name+" successfully added");
+
+        // same player name not allowed
+        for (Player name : d_playerNameList) {
+            if (name.get_playerName().equals(d_playerName)) {
+                System.out.println("Error: player " + d_playerName + " already exist");
+            }
+            return;
+        }
+
+        Player name = new Player(d_playerName);
+        d_playerNameList.add(name);
+        System.out.println("Player: " + name + " successfully added");
     }
 
+    // Method to remove player name
+    public void removePlayer(String d_playerName) {
+
+        boolean playerFound = false;
+
+        for (Player name : d_playerNameList) {
+            if (name.get_playerName().equals(d_playerName)) {
+                d_playerNameList.remove(name);
+                System.out.println("Player: " + d_playerName + " successfully removed");
+                playerFound = true;
+                break;
+            }
+        }
+
+        if (!playerFound) {
+            System.out.println("Error: Player " + d_playerName + " not found");
+        }
+    }
 }
