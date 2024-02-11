c0a6cf101aeff33d5e84121ad0f6338ed6c60afb
nitin <nitin.birdi@icloud.com>
2024-02-11 16:44:48 -0500

    edit intial commit: player name added

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,2 +1,31 @@
-package models;public class Player {
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    /* Each player name adding to d_playerName list */
+    private final static List<Player> d_playerNameList = new ArrayList<>();
+
+    /**/
+    public String d_playerName;
+
+    /* Defining constructor for player class */
+    public Player(String d_playerName) {
+        this.d_playerName = d_playerName;
+    }
+
+    /* getting player name */
+    public String get_playerName() {
+        return d_playerName;
+    }
+
+    /* Method to Add player */
+    public void setD_PlayerName(String d_playerName) {
+      Player name = new Player(d_playerName);
+      d_playerNameList.add(name);
+      System.out.println("Player name "+name+" successfully added");
+    }
+
 }
