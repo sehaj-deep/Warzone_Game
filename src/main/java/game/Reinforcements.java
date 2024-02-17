1940ae5b375fec72855159fca95850adca3a6455
Vaibhav <vaibhav.sharma17649@gmail.com>
2024-02-16 21:01:10 -0500

    Assigning reinforcement armies

--- src/main/java/game/Reinforcements.java
+++ src/main/java/game/Reinforcements.java
@@ -1,55 +1,106 @@
 package game;
+
 import java.util.HashMap;
+import java.util.HashSet;
+//import java.util.List;
 import java.util.Map;
 
 import models.Continent;
+import models.Country;
 import map.GameMap;
 
 /**
- * Player class represents players in the game.
+ * Reinforcements class represents the reinforcement phase in the game.
  */
 public class Reinforcements {
-    private String name;
+    /*
+     * through object of game_state, go to the object of a particular player and get the list of countries owned by him.
+    //  * keep boolean all_conqueredCountries = true;
+    //  * loop over each continent
+    //  * get the list of countries inside each continent
+    //  * match if each country of the continent is present owned by the player (HashSet in both)
+    //  * if not, put boolean to false
+    //  * check if allCountriesConquered is true, get the bonus armies from continent 
+    //  * (Do maintain a sum of num Of armies for each player,maybe return it to somewhere)
+    //  * 
+    //  * At the start of each loop, put boolean to true
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+// i need to pass the game state as parameter , i need to update to reinforcement army to each player 
+
+
     private int d_reinforcementArmies;
-    private Map<String, Integer> d_territoriesOwned; // A Hashmap to store Map of territories owned by the player
-    private Map<String, Continent> d_continentsOwned; // A Hashmap to store Map of continents owned by the player
-    private GameMap d_gameMap; // A Reference to the game map
+    private Map<String, Country> d_countriesOwned; // A HashMap to store countries owned by the player
+    private GameMap d_gameMap; // A reference to the game map
+    Player d_player; // player Instance
 
     /**
-     * Constructor for Player class.
-     * @param name The name of the player.
+     * Constructor for Reinforcements class.
      * @param gameMap The game map object.
+     * @param player The player instance.
      */
-    public Reinforcements(String name, GameMap gameMap) {
-        this.name = name;
-        this.d_territoriesOwned = new HashMap<>();
-        this.d_continentsOwned = new HashMap<>();
+    //@param playerCountriesMap The map of player names and their assigned countries.
+    public Reinforcements(GameMap gameMap, Player p_player) {
         this.d_gameMap = gameMap;
+        this.d_player = p_player;
     }
 
-    /**
-     * Method to add a continent owned by the player.
-     * @param continentName The name of the continent.
-     */
-    public void addContinent(String continentName) {
-        Continent continent = d_gameMap.getD_Continents().get(continentName);
-        if (continent != null) {
-            d_continentsOwned.put(continentName, continent);
-        }
-    }
+    // /**
+    //  * Method to initialize countries owned by players.
+    //  * @param playerCountriesMap The map of player names and their assigned countries.
+    //  */
+    // private void initializeCountries(Map<String, List<String>> playerCountriesMap) {
+    //     for (List<String> countries : playerCountriesMap.values()) {
+    //         for (String countryName : countries) {
+    //             addCountry(countryName);
+    //         }
+    //     }
+    // }
+
+    // /**
+    //  * Method to add a country owned by the player.
+    //  * @param countryName The name of the country.
+    //  */
+    // public void addCountry(String countryName) {
+    //     Country country = d_gameMap.getCountries().get(countryName);
+    //     if (country != null) {
+    //         d_countriesOwned.put(countryName, country);
+    //     }
+    // }
 
     /**
      * Method to calculate reinforcement armies for the player.
      */
     public void calculateReinforcementArmies() {
-        d_reinforcementArmies = d_territoriesOwned.size() / 3; // Basic calculation based on territories owned
+        d_reinforcementArmies = d_countriesOwned.size() / 3; // Basic calculation based on territories owned
 
         // Add bonus armies from continents owned
-        for (Continent continent : d_continentsOwned.values()) {
-            d_reinforcementArmies += continent.getBonusArmies();
+        for (HashMap.Entry<String, Continent> l_continent : d_gameMap.getD_Continents().entrySet()) {
+
+            HashSet<Country> l_countries = l_continent.getD_countries();
+            boolean ownsAllCountries = true;
+            for (Country country : l_countries) {
+                if (!d_player.getOwnership().contains(country.getName())) {
+                      ownsAllCountries = false;
+                  break;
+                }
+            }
+            if (ownsAllCountries) {
+                // Get the bonus armies for the continent from the game map
+                int bonusArmies = l_continent.getD_continentValue();
+                d_reinforcementArmies += bonusArmies;
+            }
         }
 
-        // it Ensure minimum reinforcement armies
+        // Ensure minimum reinforcement armies
         if (d_reinforcementArmies < 3) {
             d_reinforcementArmies = 3;
         }
@@ -63,3 +114,4 @@ public class Reinforcements {
         return d_reinforcementArmies;
     }
 }
+
