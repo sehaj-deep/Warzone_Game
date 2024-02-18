5d26c0464aef41135729020102db3216dde37651
sehaj-deep <sehajdeep490@gmail.com>
2024-02-18 14:24:28 -0500

    New Exception Class Created

---
+++ src/main/java/utils/ValidationException.java
@@ -0,0 +1,31 @@
+package utils;
+
+/**
+ * User defined Exception to handle exceptions of the game
+ */
+public class ValidationException extends Exception {
+
+	/**
+	 * Default constructor
+	 */
+	public ValidationException() {
+		super("Validation Exception occured");
+	}
+
+	/**
+	 * Parameterized constructor
+	 * 
+	 * @param message The message to display
+	 */
+	public ValidationException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Method to display the message
+	 */
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+}
