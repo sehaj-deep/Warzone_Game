3756dea08edd01609f0accdb05e269652391883d
Vaibhav <vaibhav.sharma17649@gmail.com>
2024-02-18 21:29:43 -0500

    Assigning reinforcement armies phase for all players

--- src/main/java/phases/ReinforcePhase.java
+++ src/main/java/phases/ReinforcePhase.java
@@ -1,17 +1,19 @@
 package phases;
-
 import game.GameState;
 import game.Player;
 import game.Reinforcements;
+import map.MapEditor;
 
 /**
  * ReinforcePhase class represents the reinforcement phase in the game.
  */
 public class ReinforcePhase {
-    public void execute(GameState p_state){
+		public ReinforcePhase() {}
+	
+    public void execute(GameState p_state, MapEditor p_mapEditor){
         for(Player p_player: p_state.getPlayers())
         {
-            Reinforcements l_reinforcements = new Reinforcements(p_state.getGameMap(),p_player);
+            Reinforcements l_reinforcements = new Reinforcements(p_mapEditor, p_player);
 
             l_reinforcements.calculateReinforcementArmies();
 
@@ -23,4 +25,3 @@ public class ReinforcePhase {
         }
 
     }
-
