3965220de4994d3c8fb1a5e51356473801044fa7
chihoon1 <89762947+chihoon1@users.noreply.github.com>
2024-02-18 19:48:02 -0500

    added reinforce phase

---
+++ src/main/java/phases/ReinforcePhase.java
@@ -0,0 +1,28 @@
+package phases;
+import game.GameState;
+import game.Player;
+import game.Reinforcements;
+import map.MapEditor;
+
+/**
+ * ReinforcePhase class represents the reinforcement phase in the game.
+ */
+public class ReinforcePhase {
+		public ReinforcePhase() {}
+	
+    public void execute(GameState p_state, MapEditor p_mapEditor){
+        for(Player p_player: p_state.getPlayers())
+        {
+            Reinforcements l_reinforcements = new Reinforcements(p_mapEditor, p_player);
+
+            l_reinforcements.calculateReinforcementArmies();
+
+            int reinforcementArmies = l_reinforcements.getReinforcementArmies();
+
+            p_state.getReinforcements().add(reinforcementArmies);
+
+            }
+        }
+
+    }
+
