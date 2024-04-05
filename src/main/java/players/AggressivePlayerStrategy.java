76231b774adea39371a99e4099d6135c132d7e82
chihoon1 <89762947+chihoon1@users.noreply.github.com>
2024-04-05 15:42:16 -0400

    added Aggressive and Benevolent players

---
+++ src/main/java/players/AggressivePlayerStrategy.java
@@ -0,0 +1,273 @@
+package players;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import game.GameEngine;
+import map.Country;
+import orders.Advance;
+import orders.Deploy;
+import orders.Order;
+
+public class AggressivePlayerStrategy extends PlayerStrategy {
+	private boolean d_canDeploy = true;
+	private boolean d_canAttack = true;
+	private boolean d_hasOrder = true;
+	private String d_strongest = null;
+
+	/**
+	 * Queue of orders waiting to be added to player's order's list
+	 */
+	private Queue<Order> d_waitingOrders = new LinkedList<>();
+
+	/**
+	 * Queue of orders waiting to be added to player's order's list
+	 */
+	private Queue<Order> d_remainingOrders = new LinkedList<>();
+
+	@Override
+	protected Order createOrder(Player p_player, String[] p_tokens, GameEngine p_gameEngine) {
+		int l_playerIdx = p_gameEngine.getPlayers().indexOf(p_player);
+		Order l_order = null;
+		boolean l_currPhasePtr;
+		if (d_canDeploy) {
+			l_order = deployAggressive(p_player, p_gameEngine);
+			l_currPhasePtr = d_canDeploy;
+		}
+		else if (d_canAttack) {
+			l_order = orderAttack(p_player, p_gameEngine);
+			l_currPhasePtr = d_canAttack;
+		}
+		else if (d_hasOrder) { // After deploy, now move armies to weaker countries
+			// No need to create card Order because benevolent player will never get a card
+			// as the player does not attack
+			if (d_waitingOrders.size() == 0) {
+				// has not issued Advance orders yet, so add all Advance orders to waitingOrders
+				moveToStrongestNeighbor(p_player, p_gameEngine);
+			}
+			if (d_waitingOrders.size() > 0) {
+				l_order = d_waitingOrders.remove();
+			}
+			if (d_waitingOrders.size() == 0) {
+				// if d_waitingOrder's size becomes 0 after remove,
+				// then no more Orders from this player
+				d_hasOrder = false;
+			}
+		}
+		if (l_order != null && l_order.isValidIssue(l_playerIdx)) {
+			return l_order;
+		}
+		else {
+			// if the order is not valid, then ignore and skip the order
+			return null;
+		}
+	}
+
+	/**
+	 * Deploy all the reinforcement armies to the strongest country of the player
+	 * 
+	 * @param p_player     player whom this strategy belongs to
+	 * @param p_gameEngine current game engine where this game runs
+	 * @return
+	 */
+	public Order deployAggressive(Player p_player, GameEngine p_gameEngine) {
+		int l_playerIdx = p_gameEngine.getPlayers().indexOf(p_player);
+		findStrongestCountry(p_player, p_gameEngine);
+		d_canDeploy = false;
+		return new Deploy(d_strongest, p_gameEngine.getReinforcements().get(l_playerIdx), p_gameEngine);
+	}
+
+	/**
+	 * Find the strongest country bordering enemy
+	 * 
+	 * @param p_player     player whom this strategy belongs to
+	 * @param p_gameEngine current game engine where this game runs
+	 */
+	public void findStrongestCountry(Player p_player, GameEngine p_gameEngine) {
+		String l_strongest = ""; // name of the strongest country found so far
+		int l_mostNumArmy = 0; // number of armies in the strongest country found so far
+		for (String l_countryName : p_player.getOwnership()) {
+			int l_numArmy = p_gameEngine.getGameBoard().get(l_countryName);
+			// To attack from the strongest, the country needs to border enemy
+			boolean l_isBorder = false;
+			Country l_country = p_gameEngine.getD_countries(l_countryName);
+			for (Country l_neighbor : l_country.getNeighbors()) {
+				if (!p_player.getOwnership().contains(l_neighbor.getD_name())) {
+					l_isBorder = true;
+					break;
+				}
+			}
+			// Strongest iff a country has most army and located in the border
+			if (l_isBorder && l_numArmy >= l_mostNumArmy) {
+				l_mostNumArmy = l_numArmy;
+				l_strongest = l_countryName;
+			}
+		}
+		d_strongest = l_strongest;
+	}
+
+	public Order orderAttack(Player p_player, GameEngine p_gameEngine) {
+		Country l_country = p_gameEngine.getD_countries(d_strongest);
+		// Find the weakest enemy territory in terms of number of army
+		int l_fewestNumArmy = 999999999;
+		String l_weakestEnemy = "";
+		for (Country l_neighbor : l_country.getNeighbors()) {
+			int l_numArmy = p_gameEngine.getGameBoard().get(l_neighbor.getD_name());
+			if (l_numArmy <= l_fewestNumArmy) {
+				l_fewestNumArmy = l_numArmy;
+				l_weakestEnemy = l_neighbor.getD_name();
+			}
+		}
+		d_canAttack = false;
+		// send all armies except one from my strongest to opponent's weakest for attack
+		// one army needs to stay in a country
+		int l_myNumArmy = p_gameEngine.getGameBoard().get(d_strongest) - 1;
+		return new Advance(d_strongest, l_weakestEnemy, l_myNumArmy, p_gameEngine);
+	}
+
+	/**
+	 * Read d_strongest data member of this class
+	 * 
+	 * @return the name of the strongest country owned by this player
+	 */
+	public String getStrongest() {
+		return d_strongest;
+	}
+
+	/**
+	 * Update d_strongest data member of this class
+	 * 
+	 * @param p_strongest a new name of the strongest country
+	 */
+	public void setStrongest(String p_strongest) {
+		d_strongest = p_strongest;
+	}
+
+	/**
+	 * Move armies to the strongest neighbor of the country for all countries owned
+	 * by this player. Strong country means more armies compared to a neighboring
+	 * country. This function will create Advance object for one move from a country
+	 * to its neighbor, and add that Advance object to d_waitingOrders queue
+	 * 
+	 * @param p_player     player whom this strategy belongs to
+	 * @param p_gameEngine current game engine where this game runs
+	 */
+	public void moveToStrongestNeighbor(Player p_player, GameEngine p_gameEngine) {
+		for (String l_countryName : p_player.getOwnership()) {
+			Country l_country = p_gameEngine.getD_countries(l_countryName);
+			// max number of army initialized as this country's army because the strongest
+			// neighbor needs to have more armies than this country to move
+			int l_maxNumArmy = p_gameEngine.getGameBoard().get(l_countryName);
+			String l_strongestNeighborName = null;
+			for (Country l_neighbor : l_country.getNeighbors()) {
+				if (p_player.getOwnership().contains(l_neighbor.getD_name())) {
+					int l_numArmyNeighbor = p_gameEngine.getGameBoard().get(l_neighbor.getD_name());
+					if (l_maxNumArmy < l_numArmyNeighbor) { // such neighbor is a stronger country
+
+						l_maxNumArmy = l_numArmyNeighbor;
+						l_strongestNeighborName = l_neighbor.getD_name();
+					}
+
+				}
+
+			}
+			if (l_strongestNeighborName != null) {
+				// l_country will send all armies except 1 to the strongest neighbor
+				// validation of the order will be done in create order for efficiency
+				int l_numArmy = p_gameEngine.getGameBoard().get(l_countryName) - 1;
+				Advance l_order = new Advance(l_countryName, l_strongestNeighborName, l_numArmy, p_gameEngine);
+				d_waitingOrders.add(l_order);
+			}
+		}
+	}
+
+	/**
+	 * Read d_waitingOrders data member of this class
+	 * 
+	 * @return a queue of Order objects to be added to the player's orders list
+	 */
+	public Queue<Order> getWaitingOrders() {
+		return d_waitingOrders;
+	}
+
+	/**
+	 * Read d_canDeploy data member of this class
+	 * 
+	 * @return a boolean representing whether this player can make Deploy order
+	 */
+	public boolean getCanDeploy() {
+		return d_canDeploy;
+	}
+
+	/**
+	 * Update d_canDeploy data member of this class
+	 * 
+	 * @param p_canDeploy a new boolean value whether this player can make Deploy
+	 *                    order
+	 */
+	public void setCanDeploy(boolean p_canDeploy) {
+		d_canDeploy = p_canDeploy;
+	}
+
+	/**
+	 * Read d_canAttack data member of this class
+	 * 
+	 * @return a boolean representing whether this player can make Attacking order
+	 */
+	public boolean getCanAttack() {
+		return d_canAttack;
+	}
+
+	/**
+	 * Update d_canAttack data member of this class
+	 * 
+	 * @param p_canAttack a new boolean value whether this player can make Attacking
+	 *                    order
+	 */
+	public void setCanAttack(boolean p_canAttack) {
+		d_canAttack = p_canAttack;
+	}
+
+	/**
+	 * Read d_hasOrder data member of this class
+	 * 
+	 * @return a boolean representing whether this player can make an Order
+	 */
+	public boolean getHasOrder() {
+		return d_hasOrder;
+	}
+
+	/**
+	 * Update d_hasOrder data member of this class
+	 * 
+	 * @param p_hasOrder a new boolean value whether this player can make an Order
+	 *                   order
+	 */
+	public void setHasOrder(boolean p_hasOrder) {
+		d_hasOrder = p_hasOrder;
+	}
+
+	@Override
+	protected Country toAttack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Country toAttackFrom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Country toMoveFrom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Country toDefend() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/players/BenevolentPlayerStrategyTest.java
@@ -0,0 +1,299 @@
+package players;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import game.GameEngine;
+import map.Country;
+import orders.Advance;
+import orders.Deploy;
+import orders.Order;
+
+/**
+ * This class is designed to do unit test of features in Player class
+ */
+public class BenevolentPlayerStrategyTest {
+	/**
+	 * List of players
+	 */
+	static Player d_player;
+
+	static BenevolentPlayerStrategy d_strategy;
+
+	/**
+	 * The game engine instance used for testing.
+	 */
+	static GameEngine d_gameEngine;
+
+	/**
+	 * The list of reinforcement army
+	 */
+	static List<Integer> d_reinforcements;
+
+	/**
+	 * The ID of the player initiating the advance order.
+	 */
+	static int d_plyrId = 0;
+
+	/**
+	 * This is the common setup for all test cases and will be run before each test
+	 */
+	@Before
+	public void before() {
+		d_player = new Player(Integer.toString(0));
+		d_strategy = new BenevolentPlayerStrategy();
+		d_player.setPlayerStrategy(d_strategy);
+		d_gameEngine = new GameEngine();
+		d_reinforcements = d_gameEngine.getReinforcements();
+		d_reinforcements.clear();
+		d_gameEngine.getPlayers().add(d_player);
+		d_reinforcements.add(9);
+		d_gameEngine.setReinforcements(d_reinforcements);
+
+		// setup map
+		// 1 -> 3, 5
+		// 2 -> 1, 4, 5
+		// 3 -> 4
+		// 4 -> 2, 3
+		// 5 -> 1, 2
+		Country l_country1, l_country2, l_country3, l_country4, l_country5;
+		l_country1 = new Country(0, "1");
+		l_country2 = new Country(1, "2");
+		l_country3 = new Country(2, "3");
+		l_country4 = new Country(3, "4");
+		l_country5 = new Country(4, "5");
+		l_country1.addNeighbors(l_country3);
+		l_country1.addNeighbors(l_country5);
+		l_country3.addNeighbors(l_country4);
+		l_country2.addNeighbors(l_country4);
+		l_country2.addNeighbors(l_country5);
+		l_country2.addNeighbors(l_country1);
+		l_country5.addNeighbors(l_country1);
+		l_country5.addNeighbors(l_country2);
+		l_country4.addNeighbors(l_country3);
+		l_country4.addNeighbors(l_country2);
+		HashMap<String, Country> l_countries = d_gameEngine.getD_countries();
+		l_countries.put("1", l_country1);
+		l_countries.put("2", l_country2);
+		l_countries.put("3", l_country3);
+		l_countries.put("4", l_country4);
+		l_countries.put("5", l_country5);
+		Set<String> l_ownedCountries = new HashSet<>(Arrays.asList("2", "3", "4"));
+		d_gameEngine.getPlayers().get(0).setOwnership(l_ownedCountries);
+
+	}
+
+	/**
+	 * Test findWeakestCountry method. Case: the weakest country is not in border.
+	 */
+	@Test
+	public void testFindWeakestContryNotInBorder() {
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 5);
+		l_board.put("3", 4);
+		l_board.put("4", 8);
+		d_gameEngine.setGameBoard(l_board);
+		d_strategy.findWeakestCountry(d_player, d_gameEngine);
+		String l_weakest = d_strategy.getWeakest();
+		assertEquals("3", l_weakest);
+	}
+
+	/**
+	 * Test findWeakestCountry method. Case: the weakest country is in border.
+	 */
+	@Test
+	public void testFindWeakestContryInBorder() {
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 2);
+		l_board.put("3", 4);
+		l_board.put("4", 3);
+		d_gameEngine.setGameBoard(l_board);
+		d_strategy.findWeakestCountry(d_player, d_gameEngine);
+		String l_weakest = d_strategy.getWeakest();
+		assertEquals("2", l_weakest);
+	}
+
+	/**
+	 * Test findWeakestCountry method. Case: All countries has no armies
+	 */
+	@Test
+	public void testFindWeakestContryAllZeroArmies() {
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 0);
+		l_board.put("3", 0);
+		l_board.put("4", 0);
+		d_gameEngine.setGameBoard(l_board);
+		d_strategy.findWeakestCountry(d_player, d_gameEngine);
+		String l_weakest = d_strategy.getWeakest();
+		HashSet<String> l_set = new HashSet<String>();
+		l_set.add("2");
+		l_set.add("3");
+		l_set.add("4");
+		assertTrue(l_set.contains(l_weakest));
+	}
+
+	/**
+	 * Test deployToWeakest method. Check whether created Deploy object has the
+	 * expected country name and number of army as its data
+	 */
+	@Test
+	public void testDeployToWeakest() {
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 2);
+		l_board.put("3", 4);
+		l_board.put("4", 3);
+		d_gameEngine.setGameBoard(l_board);
+		assertEquals(true, d_strategy.getCanDeploy());
+		Deploy l_order = (Deploy) d_strategy.deployToWeakest(d_player, d_gameEngine);
+		assertEquals("2", l_order.getCountryName());
+		int l_numAvailableArmy = d_reinforcements.get(0);
+		assertEquals(l_numAvailableArmy, l_order.getNumArmy());
+		assertEquals(false, d_strategy.getCanDeploy());
+	}
+
+	/**
+	 * Test moveToWeakerNeighbors method. Case: here, Country 4 has more armies than
+	 * its neighbors 2 and 5. Country 3 is close to 4 only. Country 2 adjacent to 4
+	 * and territories not owned by the player.
+	 */
+	@Test
+	public void testMoveToWeakerNeighbors() {
+		// set up game board
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 1);
+		l_board.put("3", 4);
+		l_board.put("4", 16);
+		l_board.put("1", 1);
+		l_board.put("5", 1);
+		d_gameEngine.setGameBoard(l_board);
+		HashSet<String> l_set = new HashSet<String>();
+		l_set.add("1");
+		l_set.add("5");
+
+		d_strategy.moveToWeakerNeighbors(d_player, d_gameEngine);
+		System.out.println("Test moveToWeakerNeighbors output: " + d_strategy.getWaitingOrders());
+		// check whether benevolent move is correct for all orders in waiting orders
+		// queue in the benevolent player strategy
+		for (int i = 0; i < d_strategy.getWaitingOrders().size(); i++) {
+			Advance l_order = (Advance) d_strategy.getWaitingOrders().remove();
+			assertEquals("4", l_order.getSourceCountry());
+			String l_neighborName = l_order.getTargetCountry();
+			assertFalse(l_set.contains(l_neighborName));
+			int l_diff = l_board.get("4") / 3 - l_board.get(l_neighborName) - l_order.getNumArmy();
+			assertEquals(0, l_diff);
+		}
+	}
+
+	/**
+	 * Test moveToWeakerNeighbors method. Case: All countries has no armies. Expect
+	 * no order in waitingOrders queue in the BenevolentPlayerStrategy object
+	 */
+	@Test
+	public void testMoveToWeakerNeighborsAllZeroArmies() {
+		// set up game board
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 0);
+		l_board.put("3", 0);
+		l_board.put("4", 0);
+		l_board.put("1", 10);
+		l_board.put("5", 10);
+		d_gameEngine.setGameBoard(l_board);
+		d_strategy.moveToWeakerNeighbors(d_player, d_gameEngine);
+		assertEquals(0, d_strategy.getWaitingOrders().size());
+	}
+
+	/**
+	 * Test createOrder method
+	 */
+	@Test
+	public void testCreateOrder() {
+		// set up game board
+
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 1);
+		l_board.put("3", 4);
+		l_board.put("4", 16);
+		l_board.put("1", 1);
+		l_board.put("5", 1);
+		d_gameEngine.setGameBoard(l_board);
+
+		// test deploy step
+		assertEquals(true, d_strategy.getCanDeploy());
+		Order l_order = d_strategy.createOrder(d_player, null, d_gameEngine);
+		Deploy l_deploy = new Deploy("placeholder", 1, d_gameEngine);
+		assertTrue(l_order.getClass() == l_deploy.getClass());
+		assertEquals(false, d_strategy.getCanDeploy());
+
+		// test army movement step
+		assertEquals(0, d_strategy.getWaitingOrders().size());
+		int i = 0;
+		do {
+			l_order = d_strategy.createOrder(d_player, null, d_gameEngine);
+			if (i == 0) {
+				assertTrue(d_strategy.getWaitingOrders().size() > 0);
+			}
+			Advance l_advance = new Advance("placeholder", "example", 1, d_gameEngine);
+			assertTrue(l_order.getClass() == l_advance.getClass());
+			i++;
+		}
+		while (d_strategy.getWaitingOrders().size() == 0);
+
+	}
+
+	/**
+	 * Test createOrder method. Special Case: All countries has no armies. Expect no
+	 * order in waitingOrders queue in the BenevolentPlayerStrategy object
+	 */
+	@Test
+	public void testCreateOrderAllZeroArmies() {
+		// set up game board
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 0);
+		l_board.put("3", 0);
+		l_board.put("4", 0);
+		l_board.put("1", 10);
+		l_board.put("5", 10);
+		d_gameEngine.setGameBoard(l_board);
+
+		// test deploy step
+		assertEquals(true, d_strategy.getCanDeploy());
+		Order l_order = d_strategy.createOrder(d_player, null, d_gameEngine);
+		Deploy l_deploy = new Deploy("placeholder", 1, d_gameEngine);
+		assertTrue(l_order.getClass() == l_deploy.getClass());
+		assertEquals(false, d_strategy.getCanDeploy());
+		// test army movement step
+		assertEquals(0, d_strategy.getWaitingOrders().size());
+		d_strategy.createOrder(d_player, null, d_gameEngine);
+		assertEquals(0, d_strategy.getWaitingOrders().size());
+	}
+
+	/**
+	 * Test createOrder method. Special Case: Invalid order created in the method.
+	 * createOrder method expects to ignore the invalid Order and return null
+	 */
+	@Test
+	public void testCreateOrderInvalidOrder() {
+		// set up game board
+		HashMap<String, Integer> l_board = new HashMap<>();
+		l_board.put("2", 1);
+		l_board.put("3", 4);
+		l_board.put("4", 16);
+		d_gameEngine.setGameBoard(l_board);
+		d_strategy.setCanDeploy(false);
+		Queue<Order> l_waitingOrders = d_strategy.getWaitingOrders();
+		l_waitingOrders.add(new Advance("3", "4", 10, d_gameEngine));
+		assertEquals(1, d_strategy.getWaitingOrders().size());
+		Order l_order = d_strategy.createOrder(d_player, null, d_gameEngine);
+		assertEquals(false, d_strategy.getHasOrder());
+		assertEquals(0, d_strategy.getWaitingOrders().size());
+		assertEquals(null, l_order);
+	}
+}
